2c2da2d23cabae4e0ccc2162c93971929cfbc5b3
Josh Gribbon <jgribbon@stevens.edu>
2016-08-01 16:28:12 -0400

    Add different branch for dev

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 let app = express();
 
-let port = 3000;
+let port = 3001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!`);
 });
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('Hello world 2');
 });
 
