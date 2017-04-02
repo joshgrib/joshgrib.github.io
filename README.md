0691f8fca5fb3a79cb62b5a69a2e45f7a01ac7a0
Josh Gribbon <jgribbon@stevens.edu>
2017-04-02 16:45:56 -0400

    Add stuff

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Code to run a personal site.
 * reactstrap
 * webpack
 * Babel
+
+## Todo
+- [x] Set up boilerplate
+- [x] Try out some UI frameworks
+- [x] Simplify components
+- [x] Set up basic skeleton
+- [ ] Add SASS
+- [ ] Set up nav buttons to switch main pages
+- [ ] Style header, footer, nav
+- [ ] Create home page for main
---
+++ scheduler.js
@@ -0,0 +1,23 @@
+courses = {
+    0:{
+        'dept':'CS',
+        'num':383,
+        'sections':{
+            'A':{
+                'M':[],
+                'T':[],
+                'W':[],
+                'R':[],
+                'F':[]
+            }
+        }
+    }
+}
+
+/*
+Start with classes with the least amount of sections, that should help keep the possible schedules down
+
+Maybe add courses and update to see possible combos with that
+
+Define required courses then list optional ones to include but also show schedules where that course would cause a conflict
+*/
