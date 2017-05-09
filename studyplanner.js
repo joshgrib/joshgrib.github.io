fe560101a0ddad8498c4525d484cb04294b0bef6
Josh Gribbon <jgribbon@stevens.edu>
2017-05-09 15:21:32 -0400

    Add study plan helper script thing, add all course info for unofficial transcript, create function to get GPA

---
+++ studyplanner.js
@@ -0,0 +1,545 @@
+let courses = {
+  2014:{
+    0:{
+      'season':'Fall',
+      'courses':[
+        {
+          'dept':'CAL',
+          'num':105,
+          'section':'O',
+          'name':'Knowledge, Nature, Culture',
+          'grade':'B+',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CH',
+          'num':115,
+          'section':'RL',
+          'name':'General Chemistry I',
+          'grade':'B',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CH',
+          'num':117,
+          'section':'I',
+          'name':'General Chemistry Lab I',
+          'grade':'A-',
+          'credits':1.00,
+          'weight':1
+        },
+        {
+          'dept':'E',
+          'num':101,
+          'section':'A',
+          'name':'Engineering Experience 1',
+          'grade':'P',
+          'credits':1.00,
+          'weight':0
+        },
+        {
+          'dept':'E',
+          'num':115,
+          'section':'LH',
+          'name':'Introduction to Programming',
+          'grade':'A',
+          'credits':2.00,
+          'weight':1
+        },
+        {
+          'dept':'E',
+          'num':120,
+          'section':'B',
+          'name':'Engineering Graphics',
+          'grade':'A',
+          'credits':1.00,
+          'weight':1
+        },
+        {
+          'dept':'E',
+          'num':121,
+          'section':'G',
+          'name':'Engineering Design I',
+          'grade':'A-',
+          'credits':2.00,
+          'weight':1
+        },
+        {
+          'dept':'H',
+          'num':183,
+          'section':'C',
+          'name':'Research Seminar I',
+          'grade':'P',
+          'credits':1.00,
+          'weight':0
+        },
+        {
+          'dept':'MA',
+          'num':123,
+          'section':'D',
+          'name':'Calc2A:Series,Vectors, Func',
+          'grade':'A-',
+          'credits':2.00,
+          'weight':1
+        },
+        {
+          'dept':'MA',
+          'num':124,
+          'section':'DD',
+          'name':'Calc2B:Cal for Func of Two',
+          'grade':'B',
+          'credits':2.00,
+          'weight':1
+        },
+        {
+          'dept':'MA',
+          'num':188,
+          'section':'A',
+          'name':'Seminar in Math Sciences',
+          'grade':'P',
+          'credits':1.00,
+          'weight':0
+        }
+      ]
+    }
+  },
+  2015:{
+    0:{
+      'season':'Spring',
+      'courses':[
+        {
+          'dept':'CAL',
+          'num':103,
+          'section':'L',
+          'name':'Writing & Communications',
+          'grade':'B',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CH',
+          'num':281,
+          'section':'B',
+          'name':'Biology and Biotechnology',
+          'grade':'C',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':188,
+          'section':'A',
+          'name':'Seminar in Computer Science',
+          'grade':'P',
+          'credits':1.00,
+          'weight':0
+        },
+        {
+          'dept':'E',
+          'num':122,
+          'section':'V',
+          'name':'Engineering Design II',
+          'grade':'A',
+          'credits':2.00,
+          'weight':1
+        },
+        {
+          'dept':'H',
+          'num':184,
+          'section':'B',
+          'name':'Research Seminar II',
+          'grade':'P',
+          'credits':1.00,
+          'weight':0
+        },
+        {
+          'dept':'MA',
+          'num':221,
+          'section':'D',
+          'name':'Differential Equations',
+          'grade':'W',
+          'credits':4.00,
+          'weight':0
+        },
+        {
+          'dept':'MGT',
+          'num':103,
+          'section':'J',
+          'name':'Intro to Entro Thinkng',
+          'grade':'A',
+          'credits':2.00,
+          'weight':1
+        },
+        {
+          'dept':'PEP',
+          'num':111,
+          'section':'RF',
+          'name':'Mechanics',
+          'grade':'B-',
+          'credits':3.00,
+          'weight':1
+        }
+      ]
+    },
+    1:{
+      'season':'Summer',
+      'courses':[
+        {
+          'dept':'HPL',
+          'num':341,
+          'section':'A',
+          'name':'Philosophy of Good and Evil',
+          'grade':'B',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'MA',
+          'num':222,
+          'section':'A',
+          'name':'Probability & Statistics',
+          'grade':'C-',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'HHS',
+          'num':325,
+          'section':'A',
+          'name':'African-American Studies',
+          'grade':'B-',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'PEP',
+          'num':151,
+          'section':'A',
+          'name':'Introduction to Astronomy',
+          'grade':'B+',
+          'credits':3.00,
+          'weight':1
+        }
+      ]
+    },
+    2:{
+      'season':'Fall',
+      'courses':[
+        {
+          'dept':'BT',
+          'num':181,
+          'section':'A',
+          'name':'Seminar in Business',
+          'grade':'P',
+          'credits':1.00,
+          'weight':0
+        },
+        {
+          'dept':'BT',
+          'num':353,
+          'section':'C',
+          'name':'Project Management',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':115,
+          'section':'CE',
+          'name':'Intro to Computer Science',
+          'grade':'A',
+          'credits':4.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':135,
+          'section':'A',
+          'name':'Discrete Structures',
+          'grade':'C+',
+          'credits':4.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':146,
+          'section':'B',
+          'name':'Intro to Web Prog & Web Dev',
+          'grade':'B-',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':284,
+          'section':'A',
+          'name':'Data Structures',
+          'grade':'A',
+          'credits':4.00,
+          'weight':1
+        },
+        {
+          'dept':'HHS',
+          'num':468,
+          'section':'EV',
+          'name':'History of the World',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'QF',
+          'num':365,
+          'section':'A',
+          'name':'Data Structure and Algos',
+          'grade':'A',
+          'credits':4.00,
+          'weight':1
+        }
+      ]
+    }
+  },
+  2016:{
+    0:{
+      'season':'Spring',
+      'courses':[
+        {
+          'dept':'CS',
+          'num':334,
+          'section':'A',
+          'name':'Automata and Computation',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':347,
+          'section':'B',
+          'name':'Software Development Process',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':385,
+          'section':'A',
+          'name':'Algorithms',
+          'grade':'A-',
+          'credits':4.00,
+          'weight':1
+        },
+        {
+          'dept':'HHS',
+          'num':479,
+          'section':'A',
+          'name':'Studies History of Tech.',
+          'grade':'A-',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'HSS',
+          'num':175,
+          'section':'A',
+          'name':'Fundamentals of Psychology',
+          'grade':'A-',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'PE',
+          'num':200,
+          'section':'A0',
+          'name':'Archery',
+          'grade':'P',
+          'credits':0.00,
+          'weight':0
+        }
+      ]
+    },
+    1:{
+      'season':'Summer',
+      'courses':[
+        {
+          'dept':'CS',
+          'num':546,
+          'section':'WS',
+          'name':'Web Programming',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'CS',
+          'num':810,
+          'section':'WS',
+          'name':'From NAND to Tetris',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        }
+      ]
+    },
+    2:{
+      'season':'Fall',
+      'courses':[
+        {
+          'dept':'CS',
+          'num':554,
+          'section':'A',
+          'name':'Web Programming II',
+          'grade':'A',
+          'credits':3.00,
+          'weight':1
+        },
+        {
+          'dept':'D',
+          'num':401,
+          'section':'A',
+          'name':'Coop Education Work Term 1',
+          'grade':'P',
+          'credits':0.00,
+          'weight':1
+        }
+      ]
+    }
+  },
+  2017:{
+    0:{
+      'season':'Spring',
+      'courses':[
+        {
+          'dept':'CS',
+          'num':370,
+          'section':'A',
+          'name':'Creative Prob. & Team Prog.',
+          'grade':'U',
+          'credits':(3.00),
+          'weight':0
+        },
+        {
+          'dept':'CS',
+          'num':392,
+          'section':'A',
+          'name':'Systems Programming',
+          'grade':'U',
+          'credits':3.00,
+          'weight':0
+        },
+        {
+          'dept':'CS',
+          'num':496,
+          'section':'A',
+          'name':'Programming Languages',
+          'grade':'U',
+          'credits':3.00,
+          'weight':0
+        },
+        {
+          'dept':'CS',
+          'num':545,
+          'section':'W1',
+          'name':'Human-Computer Interaction',
+          'grade':'U',
+          'credits':3.00,
+          'weight':0
+        },
+        {
+          'dept':'HSS',
+          'num':371,
+          'section':'A',
+          'name':'Computers & Society',
+          'grade':'U',
+          'credits':3.00,
+          'weight':0
+        },
+        {
+          'dept':'PE',
+          'num':200,
+          'section':'B3',
+          'name':'Bowling',
+          'grade':'P',
+          'credits':0.00,
+          'weight':0
+        },
+        {
+          'dept':'SSW',
+          'num':555,
+          'section':'WS',
+          'name':'Agile Methods for Soft. Dev.',
+          'grade':'U',
+          'credits':3.00,
+          'weight':0
+        }
+      ]
+    },
+    1:{
+      'season':'Summer',
+      'courses':[
+        {
+          'dept':'CS',
+          'num':511,
+          'section':'A',
+          'name':'Concurrent Programming',
+          'grade':'U',
+          'credits':3.00,
+          'weight':0
+        },
+        {
+          'dept':'D',
+          'num':401,
+          'section':'A',
+          'name':'Coop Education Work Term 2',
+          'grade':'U',
+          'credits':0.00,
+          'weight':0
+        }
+      ]
+    }
+  }
+}
+
+let letterGradeToNumber = (grade) => {
+  let conversions = { 'A' : 4.00, 'A-': 3.67, 'B+': 3.33, 'B' : 3.00,
+    'B-': 2.67, 'C+': 2.33, 'C' : 2.00, 'C-': 1.67, 'D' : 1.00, 'F' : 0.00,
+    'P': 4.00, 'W':-1, 'U': -1};
+    if(grade in conversions){
+      return conversions[grade];
+    }else{
+      console.error(`ERROR: '${grade}' is not a valid input for letterGradeToNumber.`);
+    }
+}
+
+let getGPA = () => {
+  let point_sum = 0;
+  let hour_sum = 0;
+  let credit_sum = 0;
+  for(let i in courses){
+    let year = i;
+    for(let j in courses[year]){
+      let semObj = courses[year][j];
+      //console.log(`---------------${year} ${semObj.season}---------------`);
+      for(let k in semObj.courses){
+        let c = semObj.courses[k];
+        //console.log(`${c.dept}${c.num}${c.section}: ${c.name}`);
+        //console.log(`    Grade: ${c.grade}(${letterGradeToNumber(c.grade)}) * ${c.credits} credits * ${c.weight}`);
+        let points = letterGradeToNumber(c.grade) * c.credits * c.weight;
+        let hours = c.credits * c.weight;
+        point_sum += points;
+        hour_sum += hours;
+        credit_sum += c.credits;
+      }
+      let gpa = point_sum / hour_sum;
+      //console.log(`GPA: ${gpa}`)
+    }
+  }
+  return point_sum / hour_sum;
+}
+
+console.log('\n\n+----------+\n| Running! |\n+----------+\n\nB');
+console.log(`GPA: ${getGPA()}`)
